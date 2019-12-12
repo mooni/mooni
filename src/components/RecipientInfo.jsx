a31615f41a051e59a74545f8938eb6b430371b94
Pacien Boisson <pakokrew@gmail.com>
2019-12-12 14:49:33 +0100

    Change RecipientInfo displaying

--- src/components/RecipientInfo.jsx
+++ src/components/RecipientInfo.jsx
@@ -4,13 +4,45 @@ import { Box } from '@material-ui/core';
 import { Info } from '@aragon/ui'
 
 function RecipientInfo({ recipient }) {
+  let fullAddress = '';
+  if(recipient.owner?.address) {
+    fullAddress += recipient.owner.address;
+  }
+  if(recipient.owner?.zip) {
+    fullAddress += ', ' + recipient.owner.address;
+  }
+  if(recipient.owner?.city) {
+    fullAddress += ', ' + recipient.owner.city;
+  }
+  if(recipient.owner?.country) {
+    fullAddress += ', ' + recipient.owner.country;
+  }
+
   return (
     <Info title="Recipient">
-      <Box>{recipient.owner.name}</Box>
-      <Box>{recipient.owner.address}</Box>
-      <Box>{recipient.owner.zip} {recipient.owner.city} {recipient.owner.country}</Box>
-      <Box><b>IBAN:</b> {recipient.iban}</Box>
-      <Box><b>BIC:</b> {recipient.bic_swift}</Box>
+      <Box>
+        <b>Name:&nbsp;</b>
+        {recipient.owner.name}
+      </Box>
+
+      {fullAddress !== '' &&
+      <Box>
+        <b>Address:&nbsp;</b>
+        {fullAddress}
+      </Box>
+      }
+
+      <Box>
+        <b>IBAN:&nbsp;</b>
+        {recipient.iban}
+      </Box>
+
+      {recipient.bic_swift &&
+      <Box>
+        <b>BIC:&nbsp;</b>
+        {recipient.bic_swift}
+      </Box>
+      }
     </Info>
   )
 }
