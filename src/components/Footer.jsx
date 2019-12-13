2e6198dc7b349bddc1c38004cf06724608c4d466
Pacien Boisson <pakokrew@gmail.com>
2019-12-13 13:56:17 +0100

    Use footer on payment page

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,39 +6,13 @@ import { Grid, Box } from '@material-ui/core';
 
 function Footer() {
   return (
-    <Box pt={4}>
-      <Info mode="warning">
-        This is experimental software provided as an alpha version and is in active development. Please use with care.
-      </Info>
-      <Box textAlign="center" pt={3} pb={2}>
-        <Box fontSize={14} color="text.secondary" fontWeight="fontWeightLight" fontStyle="oblique">
-          Powered by
-        </Box>
+    <Box pt={2}>
+      <Box pb={2}>
+        <Info mode="warning">
+          This is experimental software provided as an alpha version and is in active development. Please use with care.
+        </Info>
       </Box>
-      <Grid container justify="center" alignItems="center" spacing={4} py={2}>
-        <Grid item>
-          <Box display="flex" justifyContent="center" alignItems="center">
-            <Link href="https://bity.com" external>
-              <img src="images/bity_logo_blue.svg" alt="bity.com" height={30} />
-            </Link>
-          </Box>
-        </Grid>
-        {/*<Grid item xs={12} sm>
-          <Box display="flex" justifyContent="center" alignItems="center">
-            <Link href="https://dex.ag" external>
-              <img src="images/dexag.svg" alt="dex.ag" height={30} />
-            </Link>
-          </Box>
-        </Grid>*/}
-        <Grid item>
-          <Box display="flex" justifyContent="center" alignItems="center">
-            <Link href="https://3box.io" external>
-              <img src="images/3box.svg" alt="3box.io" height={30} />
-            </Link>
-          </Box>
-        </Grid>
-      </Grid>
-      <Grid container justify="center" alignItems="center" spacing={4} py={2}>
+      <Grid container justify="center" alignItems="center" spacing={4}>
         <Grid item>
           <Box display="flex" justifyContent="center" alignItems="center">
             <Link href="https://mooni.launchaco.com" external style={{ textDecoration: 'none' }}>
