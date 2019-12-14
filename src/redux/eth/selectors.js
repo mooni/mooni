b84e445bd83af0ef4e80ea96f730c2431eeb9c6b
Pacien Boisson <pakokrew@gmail.com>
2019-12-14 16:23:53 +0100

    clean

--- src/redux/eth/selectors.js
+++ src/redux/eth/selectors.js
@@ -1,7 +1,7 @@
 import { selectProperty } from '../helpers';
 import { STATE_NAME } from './reducer';
 
-export const getETHManager = selectProperty([STATE_NAME, 'ethManager']); // TODO rename
-export const getETHManagerLoading = selectProperty([STATE_NAME, 'ethManagerLoading']); // TODO rename
-export const getLoginModalOpen = selectProperty([STATE_NAME, 'loginModalOpen']); // TODO rename
+export const getETHManager = selectProperty([STATE_NAME, 'ethManager']);
+export const getETHManagerLoading = selectProperty([STATE_NAME, 'ethManagerLoading']);
+export const getLoginModalOpen = selectProperty([STATE_NAME, 'loginModalOpen']);
 export const getAddress = selectProperty([STATE_NAME, 'address']);
