282c605365b44de30544fd080e3e01608299efd8
Pacien Boisson <pakokrew@gmail.com>
2020-04-13 10:14:00 +0200

    Display message in case app cannot load

--- src/index.js
+++ src/index.js
@@ -4,13 +4,15 @@ import ReactDOM from 'react-dom';
 import * as serviceWorker from './serviceWorker';
 import App from './App';
 
+const incompatibleBrowserElement = document.getElementById('incompatible-browser');
+if(incompatibleBrowserElement) {
+  incompatibleBrowserElement.parentNode.removeChild(incompatibleBrowserElement)
+}
+
 const Root = (
   <App/>
 );
 
 ReactDOM.render(Root, document.getElementById('root'));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+serviceWorker.unregister(); // https://bit.ly/CRA-PWA
