9b65a8d1ba1788320ad7f6a693fd6f7431baf2ae
Pacien Boisson <pakokrew@gmail.com>
2020-04-28 18:14:49 +0200

    Update host example app

--- packages/example-host/src/App.js
+++ packages/example-host/src/App.js
@@ -1,16 +1,23 @@
-import React from 'react';
+import React, { useCallback } from 'react';
 import './App.css';
 
 import MooniWidget from '@mooni/widget';
 
+const mooni = new MooniWidget();
+
 function App() {
+
+  const openMooni = useCallback(() => {
+    mooni.open();
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
         My awesome marketplace
       </header>
       <main className="App-main">
-        <button className="open-widget" onClick={() => MooniWidget.open()}>Cash out</button>
+        <button className="open-widget" onClick={openMooni}>Cash out</button>
       </main>
     </div>
   );
