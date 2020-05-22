462e553f7389471857fdd9ffef2748d65629bb54
Pacien Boisson <pakokrew@gmail.com>
2020-05-22 16:05:21 +0200

    updated react ga event for source app

--- packages/app/src/lib/analytics.js
+++ packages/app/src/lib/analytics.js
@@ -14,7 +14,8 @@ if(process.env.NODE_ENV === 'production') {
   );
   ReactGA.initialize('UA-68373171-8');
   ReactGA.event({
-    category: 'app',
+    category: 'source',
+    action: 'app',
   });
   LogRocket.getSessionURL(function (sessionURL) {
     ReactGA.event({
