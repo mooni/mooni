005b6bf0c0bc74278c936fe666fc823eb6900642
Pacien Boisson <pakokrew@gmail.com>
2020-12-13 19:13:20 +0100

    initialEmptyState

--- app/src/redux/payment/reducer.ts
+++ app/src/redux/payment/reducer.ts
@@ -204,8 +204,8 @@ const initialMockStateComplete: PaymentState = {
   },
 };
 
-// const initialState = initialEmptyState;
-const initialState = initialMockStateMinimum;
+const initialState = initialEmptyState;
+// const initialState = initialMockStateMinimum;
 
 export default function(state : PaymentState = initialState, action: { type: string, payload?: any }): PaymentState {
   switch (action.type) {
