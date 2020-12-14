ea40c054556120c42bd4de095d6008f7b741c426
Pacien Boisson <pakokrew@gmail.com>
2020-12-14 12:41:29 +0100

    fix decimals

--- app/src/lib/trading/currencyList.ts
+++ app/src/lib/trading/currencyList.ts
@@ -14,8 +14,8 @@ export const cryptoCurrencies = [
 ];
 export const tokenCurrenciesAllNetwork = [
   new Token(18, '0x6B175474E89094C44Da98b954EedeAC495271d0F', ChainId.MAINNET, 'DAI'),
-  new Token(18, '0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48', ChainId.MAINNET, 'USDC'),
-  new Token(18, '0x2260FAC5E5542a773Aa44fBCfeDf7C193bc2C599', ChainId.MAINNET, 'WBTC'),
+  new Token(6, '0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48', ChainId.MAINNET, 'USDC'),
+  new Token(8, '0x2260FAC5E5542a773Aa44fBCfeDf7C193bc2C599', ChainId.MAINNET, 'WBTC'),
   new Token(18, '0x2448eE2641d78CC42D7AD76498917359D961A783', ChainId.RINKEBY, 'DAI'),
 ];
 export const tokenCurrencies = tokenCurrenciesAllNetwork.filter(t => t.chainId === config.chainId) as Token[];
