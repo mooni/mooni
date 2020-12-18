7aa2813adb3a59e6416542ef0ea240e8ce1ecede
Pacien Boisson <pakokrew@gmail.com>
2020-12-18 20:00:49 +0100

    Squash migrations

--- app/prisma/migrations/20201218185907_init/migration.sql
+++ app/prisma/migrations/20201218185907_init/migration.sql
@@ -1,5 +1,23 @@
--- AlterTable
-ALTER TABLE "MooniOrder" ADD COLUMN     "referalId" TEXT;
+-- CreateEnum
+CREATE TYPE "OrderStatus" AS ENUM ('PENDING', 'EXECUTED');
+
+-- CreateTable
+CREATE TABLE "MooniOrder" (
+    "id" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "executedAt" TIMESTAMP(3),
+    "status" "OrderStatus" NOT NULL DEFAULT E'PENDING',
+    "ethAddress" TEXT NOT NULL,
+    "inputAmount" TEXT NOT NULL,
+    "inputCurrency" TEXT NOT NULL,
+    "outputAmount" TEXT NOT NULL,
+    "outputCurrency" TEXT NOT NULL,
+    "ethAmount" TEXT NOT NULL,
+    "bityOrderId" TEXT,
+    "referalId" TEXT,
+
+    PRIMARY KEY ("id")
+);
 
 -- CreateTable
 CREATE TABLE "User" (
@@ -10,6 +28,9 @@ CREATE TABLE "User" (
 
     PRIMARY KEY ("id")
 );
+
+-- CreateIndex
+CREATE UNIQUE INDEX "MooniOrder.bityOrderId_unique" ON "MooniOrder"("bityOrderId");
 
 -- CreateIndex
 CREATE UNIQUE INDEX "User.ethAddress_unique" ON "User"("ethAddress");
