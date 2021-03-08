774d82dc827f9612817c382e73330f5ff8d5a3bb
Pacien Boisson <pakokrew@users.noreply.github.com>
2021-03-08 12:09:58 +0400

    Remove 3box (#145)

--- app/src/components/Order/StepRecipient.jsx
+++ app/src/components/Order/StepRecipient.jsx
@@ -1,4 +1,4 @@
-import React, { useMemo, useState } from 'react';
+import React, { useMemo } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 
 import Box from '@material-ui/core/Box';
@@ -6,64 +6,34 @@ import { IconArrowRight } from '@aragon/ui'
 
 import {setRecipient, setReference} from '../../redux/payment/actions';
 import {getRecipient, getReference} from '../../redux/payment/selectors';
-// import { getBoxManager } from '../../redux/box/selectors';
-// import { updateMyAccount } from '../../redux/contacts/actions';
 
 import RecipientForm from './RecipientForm';
-import {BoxLoadingContainer, BoxModal} from '../Utils/RequireBox';
 import {RoundButton} from "../UI/StyledComponents";
 
-// const useStyles = makeStyles(() => ({
-//   saveInfoRow: {
-//     marginBottom: '15px',
-//     display: 'flex',
-//     alignItems: 'center',
-//   },
-// }));
-
 function StepRecipient({ onComplete }) {
-  // const classes = useStyles();
   const recipient = useSelector(getRecipient);
   const reference = useSelector(getReference);
-  // const boxManager = useSelector(getBoxManager);
   const dispatch = useDispatch();
-  // const [saveAccountInfosaveAccountInfo, setSaveAccountInfo] = useState(false);
-  const [showBoxModal, setShowBoxModal] = useState(false);
 
   async function onSubmit(data) {
     dispatch(setRecipient(data.recipient));
     dispatch(setReference(data.reference));
-    // if(saveAccountInfo) {
-    //   await dispatch(updateMyAccount(data));
-    // }
     onComplete();
   }
 
-  /*
-  function onClickSaveData(value) {
-    if(boxManager) {
-      setSaveAccountInfo(!saveAccountInfo);
-    } else {
-      setShowBoxModal(true);
-    }
-  }
-   */
   const initialData = useMemo(() => ({
     recipient,
     reference,
   }), [recipient, reference]);
 
   return (
-    <BoxLoadingContainer>
-      <Box width={1}>
-        <BoxModal visible={showBoxModal} onClose={() => setShowBoxModal(false)}/>
-
-        <RecipientForm
-          initialData={initialData}
-          onSubmit={onSubmit}
-          endComponent={({ submit, isValid }) => (
-            <Box mt={2}>
-              {/*<label className={classes.saveInfoRow}>
+    <Box width={1}>
+      <RecipientForm
+        initialData={initialData}
+        onSubmit={onSubmit}
+        endComponent={({ submit, isValid }) => (
+          <Box mt={2}>
+            {/*<label className={classes.saveInfoRow}>
                 <Checkbox
                   checked={saveAccountInfo}
                   onChange={onClickSaveData}
@@ -74,12 +44,11 @@ function StepRecipient({ onComplete }) {
                   The data is stored encrypted in a decentralized storage, so only you have access to it.
                 </Help>
               </label>*/}
-              <RoundButton mode="strong" onClick={submit} wide icon={<IconArrowRight/>} label="Next" disabled={!isValid} />
-            </Box>
-          )}
-        />
-      </Box>
-    </BoxLoadingContainer>
+            <RoundButton mode="strong" onClick={submit} wide icon={<IconArrowRight/>} label="Next" disabled={!isValid} />
+          </Box>
+        )}
+      />
+    </Box>
   )
 }
 
