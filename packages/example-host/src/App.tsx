30f6b116d08ab20a5d56002da156512ad25d282d
Pacien Boisson <pakokrew@gmail.com>
2021-03-12 18:55:24 +0400

    Update example host and style

--- packages/example-host/src/App.tsx
+++ packages/example-host/src/App.tsx
@@ -1,11 +1,21 @@
 import React from 'react';
 import './App.css';
 import Home from './Home'
+import { Web3ReactProvider } from '@web3-react/core'
+import { Web3Provider } from '@ethersproject/providers'
+
+
+function getLibrary(provider, connector) {
+  return new Web3Provider(provider) // this will vary according to whether you use e.g. ethers or web3.js
+}
 
 function App() {
   return (
     <>
-      <Home/>
+      <Web3ReactProvider getLibrary={getLibrary}>
+        <Home/>
+      </Web3ReactProvider>
+
     </>
   );
 }
